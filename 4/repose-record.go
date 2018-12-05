058701d2f12ae88c26dbdc3d1219c6a340d938ca
Stefano Angaran <stefano.angaran@upyou.it>
2018-12-06 00:38:34 +0100

    Solved day 4 first part

---
+++ 4/repose-record.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func readAndSortInput() ([]string, error) {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	lines := []string{}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	sort.Strings(lines)
+
+	return lines, scanner.Err()
+}
+
+func findSleepyGuard(m map[int][]int) int {
+	sleepy := 0
+	maxSleptMinutes := 0
+	for guardId, minutes := range m {
+		if len(minutes) > maxSleptMinutes {
+			sleepy = guardId
+			maxSleptMinutes = len(minutes)
+		}
+	}
+
+	return sleepy
+}
+
+func findMostSleptMinute(minutes []int) int {
+	mostSleptMinute := 0
+	maxSlept := 0
+
+	minutesMap := make(map[int]int)
+
+	for _, m := range minutes {
+		minutesMap[m]++
+		if minutesMap[m] > maxSlept {
+			maxSlept = minutesMap[m]
+			mostSleptMinute = m
+		}
+	}
+
+	return mostSleptMinute
+}
+
+func main() {
+	lines, err := readAndSortInput()
+
+	guardsMap := make(map[int][]int)
+
+	r, _ := regexp.Compile("[[0-9]{4}-([0-9]{2})-([0-9]{2}) ([0-9]{2}):([0-9]{2})] (.+)")
+	s, _ := regexp.Compile("#([0-9]+)")
+
+	currentGuard := 0
+	startsSleeping := 0
+	for _, line := range lines {
+		lineMatches := r.FindStringSubmatch(line)
+		guardMatches := s.FindStringSubmatch(lineMatches[5])
+
+		minute, _ := strconv.Atoi(lineMatches[4])
+		if guardMatches != nil {
+			// found a guard ID
+			currentGuard, _ = strconv.Atoi(guardMatches[1])
+		}
+
+		// toggle sleeping status
+		if strings.Contains(lineMatches[5], "falls") {
+			startsSleeping = minute
+		} else if strings.Contains(lineMatches[5], "wakes") {
+			// update sleeping minutes
+			currentMinute, _ := strconv.Atoi(lineMatches[4])
+			for i := startsSleeping; i < currentMinute; i++ {
+				guardsMap[currentGuard] = append(guardsMap[currentGuard], i)
+			}
+		}
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// find the guard that sleeps the most
+	sleepyGuard := findSleepyGuard(guardsMap)
+	mostSleptMinute := findMostSleptMinute(guardsMap[sleepyGuard])
+
+	fmt.Println("Guard with more slept minutes:", sleepyGuard)
+	fmt.Println("The most slept minute of this guard is:", mostSleptMinute)
+	fmt.Println("Product of the two values is:", sleepyGuard*mostSleptMinute)
+}
