ec40ca86d6b72e782197484d5798918627e1f7e4
Stefano Angaran <stefano.angaran@upyou.it>
2018-12-08 21:30:27 +0100

    Solved day 6 part 1

---
+++ 6/chronal-coordinates.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func ReadCoordinatesFromFile() ([][]int, error) {
+	file, err := os.Open("input.txt")
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var coords [][]int
+
+	for scanner.Scan() {
+		tmp := strings.Split(scanner.Text(), ", ")
+
+		x, _ := strconv.Atoi(tmp[0])
+		y, _ := strconv.Atoi(tmp[1])
+
+		xy := []int{x, y}
+		coords = append(coords, xy)
+	}
+
+	return coords, scanner.Err()
+}
+
+func Abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func ManhattanDistance(x1 int, y1 int, x2 int, y2 int) int {
+	return Abs(x1-x2) + Abs(y1-y2)
+}
+
+func FindClosestLocation(locations [][]int, x int, y int) int {
+	closestLocation := -1
+	closestDistance := -1
+	secondClosestDistance := -1
+	for index, coords := range locations {
+		dst := ManhattanDistance(x, y, coords[0], coords[1])
+
+		if closestDistance < 0 || dst <= closestDistance {
+			secondClosestDistance = closestDistance
+			closestDistance = dst
+			closestLocation = index
+		}
+	}
+
+	if secondClosestDistance == closestDistance {
+		// if two or more locations are closest to this point
+		// then we return -1
+		return -1
+	}
+
+	return closestLocation
+}
+
+func MaxNonInfinite(a []int, infiniteLocations []int) int {
+	max := 0
+	for pos, n := range a {
+		if n > max && Index(infiniteLocations, pos) < 0 {
+			max = n
+		}
+	}
+
+	return max
+}
+
+func Index(haystack []int, needle int) int {
+	for index, element := range haystack {
+		if element == needle {
+			return index
+		}
+	}
+
+	return -1
+}
+
+func BottomRightPoint(coords [][]int) (x int, y int) {
+	for _, point := range coords {
+		if point[0] > x {
+			x = point[0]
+		}
+		if point[1] > y {
+			y = point[1]
+		}
+	}
+
+	return x, y
+}
+
+func main() {
+	locations, err := ReadCoordinatesFromFile()
+
+	infiniteLocations := []int{}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	areas := make([]int, len(locations))
+	right, bottom := BottomRightPoint(locations)
+
+	for i := 0; i < bottom; i++ {
+		for j := 0; j < right; j++ {
+			closestLocation := FindClosestLocation(locations, j, i)
+			if closestLocation >= 0 {
+				areas[closestLocation]++
+
+				if (i == 0 || j == 0 || i == bottom || j == right) && Index(infiniteLocations, closestLocation) < 0 {
+					// we are on the border so the retrieved location
+					// has infinite area
+					infiniteLocations = append(infiniteLocations, closestLocation)
+				}
+			}
+		}
+	}
+
+	fmt.Println("The maximum non-infinite area is:", MaxNonInfinite(areas, infiniteLocations))
+}
